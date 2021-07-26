523304fa6388a84ed7ceb24f5967fdab321b81c6
BonusGned <jesus.moj@mail.ru>
2021-07-26 16:34:20 +0300

    new

---
+++ users/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers, exceptions
+from django.contrib.auth import password_validation
+from users.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password', 'date_joined')
+
+    def validate_password(self, password):
+        password_validation.validate_password(password, self.instance)
+        print(password)
+        return password
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
+
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        if validated_data.get('password'):
+            instance.set_password(validated_data.get('password'))
+        instance.save()
+
+        return instance
---
+++ users/views.py
@@ -0,0 +1,46 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
+from django.shortcuts import render
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserView(APIView):
+
+    def get(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        if request.GET:
+            try:
+                user = User.objects.get_(Q(username=request.GET.get('login')) | Q(pk=request.GET.get('id')))
+                serializer = UserSerializer(user)
+            except ObjectDoesNotExist:
+                return Response('User not found')
+        return Response({'users': serializer.data})
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+        return Response({'success': f'User created successfully'})
+
+    def put(self, request):
+        if request.GET:
+            user = get_object_or_404(User.objects.all(),
+                                     Q(username=request.GET.get('login')) | Q(pk=request.GET.get('id')))
+        serializer = UserSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+        return Response({'success': f'User updated successfully'})
+
+    def delete(self, request):
+        user = get_object_or_404(User.objects.all(),
+                                 Q(username=request.GET.get('login')) | Q(pk=request.GET.get('id')))
+        user.delete()
+        return Response({
+            'message': f'User has been deleted.'
+        }, status=204)
